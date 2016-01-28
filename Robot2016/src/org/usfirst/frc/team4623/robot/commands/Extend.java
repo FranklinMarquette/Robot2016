aff8563f260e9f660ae019912e44a307cddb2079
Franklin Marquette <mrstudofrc@outlook.com>
2016-01-28 10:47:22 -0600

    Update Extend.java

--- Robot2016/src/org/usfirst/frc/team4623/robot/commands/Extend.java
+++ Robot2016/src/org/usfirst/frc/team4623/robot/commands/Extend.java
@@ -23,7 +23,7 @@ public class Extend extends Command {
 
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
-    	Robot.pnue.extend();
+    	Robot.pnue.extend1();
     }
 
     // Make this return true when this Command no longer needs to run execute()
@@ -33,7 +33,7 @@ public class Extend extends Command {
 
     // Called once after isFinished returns true
     protected void end() {
-    	Robot.pnue.stop();
+    	Robot.pnue.stop1();
     }
 
     // Called when another command which requires one or more of the same
